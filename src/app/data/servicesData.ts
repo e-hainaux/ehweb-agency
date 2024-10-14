3ce0e09fb8ebd5e8f1e3450c191c2af350559ac3
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-10-14 14:17:05 +0200

    feat multiple services info display oK

---
+++ src/app/data/servicesData.ts
@@ -0,0 +1,34 @@
+export interface Service {
+  id: number;
+  title: string;
+  paragraph: string | string[];
+  imageUrl: string;
+}
+
+export const servicesInfo: Service[] = [
+  {
+    id: 1,
+    title: "Site monopage",
+    paragraph:
+      "Un site vitrine moderne vous permet de communiquer sur votre activité de manière simple au plus grand nombre.",
+    imageUrl: "/images/networkPicto.png",
+  },
+
+  {
+    id: 2,
+    title: "Sur mesure",
+    paragraph: [
+      "Vous avez besoin de plus de fonctionnalités ?",
+      "Maîtrisez votre communication avec un site web complet.",
+    ],
+    imageUrl: "/images/registerPicto.png",
+  },
+
+  {
+    id: 3,
+    title: "Applicatif",
+    paragraph:
+      "Application mobile ou logiciel professionnel de gestion, nous construisons ensemble selon vos besoins.",
+    imageUrl: "/images/mobileAppPicto.png",
+  },
+];
--- src/components/ServicesInfo.tsx
+++ src/components/ServicesInfo.tsx
@@ -1,30 +1,46 @@
 import React from "react";
 import styles from "@/styles/ServicesInfo.module.css";
 import Image from "next/image";
+import { servicesInfo } from "@/app/data/servicesData";
 
 export default function ServicesInfo() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.imageContainer}>
-        <Image
-          src="/images/networkPicto.png"
-          alt="Header Logo"
-          width={681}
-          height={419}
-          quality={100}
-          className={styles.picto}
-        />
-      </div>
-      <div className={styles.voidContainer}></div>
-      <div className={styles.textContainer}>
-        <div className={styles.textWrapper}>
-          <h2 className={styles.title}>Site monopage</h2>
-          <p className={styles.paragraph}>
-            Un site vitrine moderne vous permet de communiquer sur votre
-            activité de manière simple au plus grand nombre.
-          </p>
+      {servicesInfo.map((service) => (
+        <div
+          key={service.id}
+          className={`${styles.serviceContainer} ${
+            service.id % 2 === 0 ? styles.reverse : ""
+          }`}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src={service.imageUrl}
+              alt={service.title}
+              width={681}
+              height={419}
+              quality={100}
+              className={styles.picto}
+            />
+          </div>
+          <div className={styles.voidContainer}></div>
+          <div className={styles.textContainer}>
+            <div className={styles.textWrapper}>
+              <h2 className={styles.title}>{service.title}</h2>
+              <p className={styles.paragraph}>
+                {Array.isArray(service.paragraph)
+                  ? service.paragraph.map((text, index) => (
+                      <span key={index}>
+                        {text}
+                        <br />
+                      </span>
+                    ))
+                  : service.paragraph}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
