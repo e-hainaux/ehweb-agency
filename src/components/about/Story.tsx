9c6413b9229c07496e4a0ba30e7001271bd3bda8
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-10-18 16:08:49 +0200

    fix ESLint unescaped-entities

--- src/components/about/Story.tsx
+++ src/components/about/Story.tsx
@@ -56,22 +56,23 @@ export default function Story() {
         </div>
         <div className={styles.presentation}>
           <p className={styles.paragraph}>
-            Développeur web & mobile fullstack en reconversion, j'ai choisi ce
-            domaine après un bilan de compétences effectué quelques années
-            auparavant.
+            Développeur web &amp; mobile fullstack en reconversion, j&apos;ai
+            choisi ce domaine après un bilan de compétences effectué quelques
+            années auparavant.
           </p>
           <p className={styles.paragraph}>
-            Je me suis initié aux bases du développement à l'aide de tutoriels
-            et de cours en ligne. Autant attiré par l'aspect technique que
-            créatif, je me suis lancé dans une formation à La Capsule en 2024,
-            où j'ai acquis des compétences en JavaScript, Node.js, Express,
-            React et React Native.
+            Je me suis initié aux bases du développement à l&apos;aide de
+            tutoriels et de cours en ligne. Autant attiré par l'aspect technique
+            que créatif, je me suis lancé dans une formation à La Capsule en
+            2024, où j&apos;ai acquis des compétences en JavaScript, Node.js,
+            Express, React et React Native.
           </p>
           <p className={styles.paragraph}>
-            J'ai réalisé dans la foulée deux projets en React.js en tant
-            qu'indépendant. Un récent stage chez Grow Your Business a renforcé
-            ma conviction d'être sur le bon chemin. Je recherche aujourd'hui un
-            premier poste en CDI, que ce soit en backend, frontend ou fullstack.
+            J&apos;ai réalisé dans la foulée deux projets en React.js en tant
+            qu&apos;indépendant. Un récent stage chez Grow Your Business a
+            renforcé ma conviction d&apos;être sur le bon chemin. Je recherche
+            aujourd&apos;hui un premier poste en CDI, que ce soit en backend,
+            frontend ou fullstack.
           </p>
         </div>
       </div>
