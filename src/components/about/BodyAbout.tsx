c11f6eb2bbd53429bd10a1c5262c45332194679e
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-10-19 18:39:45 +0200

    feat Misc part OK

--- src/components/about/BodyAbout.tsx
+++ src/components/about/BodyAbout.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "../../styles/Body.module.css";
 import Hello from "./Hello";
 import Story from "./Story";
+import Misc from "./Misc";
 
 export default function BodyAbout() {
   return (
     <div className={styles.mainContainer}>
       <Hello />
       <Story />
+      <Misc />
     </div>
   );
 }
---
+++ src/components/about/Misc.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useRef } from "react";
+import styles from "../../styles/about/Misc.module.css";
+import { FaRegHeart } from "react-icons/fa";
+
+export default function Misc() {
+  const [isVisible, setIsVisible] = useState(false);
+  const iconRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          } else {
+            setIsVisible(false);
+          }
+        });
+      },
+      {
+        threshold: 0.6,
+      }
+    );
+
+    if (iconRef.current) {
+      observer.observe(iconRef.current);
+    }
+
+    return () => {
+      if (iconRef.current) {
+        observer.unobserve(iconRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.frameContainer}>
+        <div
+          ref={iconRef}
+          className={`${styles.pictoContainer} ${
+            isVisible ? styles.fadeIn : styles.fadeOut
+          }`}
+        >
+          <FaRegHeart size={32} color="#deb992" />{" "}
+        </div>
+        <div className={styles.textContainer}>
+          <div className={styles.subTitle}>
+            <h2>J'aime</h2>
+          </div>
+          <div className={styles.paragraphsContainer}>
+            <p className={styles.paragraph}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in
+              velit eget dolor euismod vulputate. Praesent facilisis accumsan
+              orci, at sodales quam consequat in. Sed vitae magna at nisl
+              eleifend malesuada.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
