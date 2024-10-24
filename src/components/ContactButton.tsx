bceeeaca24d2285e6489f6164bc7dd8eb4a248cb
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-10-24 17:04:09 +0200

    update services frames style + contact button to Link OK

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "../styles/ContactButton.module.css";
+import Link from "next/link";
 
 export default function ContactButton() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>Contactez-moi !</button>
+        <Link href="/contact" className={styles.link}>
+          <button className={styles.button}>Contactez-moi !</button>
+        </Link>
       </div>
     </div>
   );
